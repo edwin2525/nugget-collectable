eb18037ec1fd17e8829df29d85ba071788faf333
edwin2525 <evlasics@gmail.com>
2021-01-26 08:33:02 -0500

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,18 +1,16 @@
-url = 'https://ipapi.co/json/'
+url = 'http://ip-api.com/json?callback=?'
 
 async function get(url)
 {
 	const response = await fetch(url);
-   	const jsonResponse = await response.json();
- 	console.log(jsonResponse);
- 	document.getElementById("location").innerHTML = 
-		'It only appears for people in ' 
-		+ jsonResponse.city + ", " 
-		+ jsonResponse.regionName + ", " 
-		+ jsonResponse.country + 
-		". Lucky you! P.S. I've heard the " 
-		+ jsonResponse.isp 
-		+ " rates aren't so great...";
+    const text = await response.text();
+    parsedText=JSON.
+    parse(text
+    .replace(/^[^(]*\(/, "")
+ 	.replace(/\)[^(]*$/, "")
+ 	.split(/\)[^(]*\(/));
+ 	console.log(parsedText);
+ 	document.getElementById("location").innerHTML = 'It only appears for people in ' + parsedText.city + ", " + parsedText.regionName + ", " + parsedText.country + ". Lucky you! P.S. I've heard the " + parsedText.isp + " rates aren't so great...";
 }
 
 get(url);
