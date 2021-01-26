5cd2ecc18fccfd04d81a80c0b7d01eb15a1e16f8
edwin2525 <evlasics@gmail.com>
2021-01-26 09:23:23 -0500

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,16 +1,13 @@
-url = 'http://www.geoplugin.net/json.gp'
+url = 'https://api.ipgeolocation.io/ipgeo?apiKey=f1227ae10f134b2588b506168fabe5a8'
 
 async function get(url)
 {
 	const response = await fetch(url);
     const text = await response.text();
-    parsedText=JSON.
-    parse(text
-    .replace(/^[^(]*\(/, "")
- 	.replace(/\)[^(]*$/, "")
- 	.split(/\)[^(]*\(/));
+    const parsedText=JSON.parse(text);
  	console.log(parsedText);
- 	document.getElementById("location").innerHTML = 'It only appears for people in ' + parsedText.geoplugin_city + ", " + parsedText.geoplugin_regionName + ", " + parsedText.geoplugin_countryName + ". Lucky you!";
+ 	document.getElementById("location").innerHTML = 
+		'It only appears for people nearby ' + parsedText.district + ". Lucky you!";
 }
 
 get(url);
