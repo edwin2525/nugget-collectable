721e93876ec5d067512839852038b87e414cd825
edwin2525 <evlasics@gmail.com>
2021-01-26 08:30:53 -0500

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -5,7 +5,14 @@ async function get(url)
 	const response = await fetch(url);
    	const jsonResponse = await response.json();
  	console.log(jsonResponse);
- 	document.getElementById("location").innerHTML = 'It only appears for people in ' + jsonResponse.city + ", " + jsonResponse.regionName + ", " + jsonResponse.country + ". Lucky you! P.S. I've heard the " + jsonResponse.isp + " rates aren't so great...";
+ 	document.getElementById("location").innerHTML = 
+		'It only appears for people in ' 
+		+ jsonResponse.city + ", " 
+		+ jsonResponse.regionName + ", " 
+		+ jsonResponse.country + 
+		". Lucky you! P.S. I've heard the " 
+		+ jsonResponse.isp 
+		+ " rates aren't so great...";
 }
 
 get(url);
