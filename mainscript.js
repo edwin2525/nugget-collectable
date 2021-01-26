84a91690bc12dea8a4d7ac577b20fd4ae4ea29ce
edwin2525 <evlasics@gmail.com>
2021-01-26 08:29:22 -0500

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -3,11 +3,9 @@ url = 'https://ipapi.co/json/'
 async function get(url)
 {
 	const response = await fetch(url);
-    const text = await response.text();
-    parsedText=JSON.
-    parse(text)
- 	console.log(parsedText);
- 	document.getElementById("location").innerHTML = 'It only appears for people in ' + parsedText.city + ", " + parsedText.regionName + ", " + parsedText.country + ". Lucky you! P.S. I've heard the " + parsedText.isp + " rates aren't so great...";
+   	const jsonResponse = await response.json();
+ 	console.log(jsonResponse);
+ 	document.getElementById("location").innerHTML = 'It only appears for people in ' + jsonResponse.city + ", " + jsonResponse.regionName + ", " + jsonResponse.country + ". Lucky you! P.S. I've heard the " + jsonResponse.isp + " rates aren't so great...";
 }
 
 get(url);
